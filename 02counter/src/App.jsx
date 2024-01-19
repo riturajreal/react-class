e2a60560f4e8b1fced04dda12c0b1ef97de83f8c
Rituraj Jha <riturajreal01@gmail.com>
2024-01-19 20:49:09 +0530

    completed 02 counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // use state
+  let [counter, setCounter] = useState(15);
+
+  // increment
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    console.log("value added", Math.random());
+  }
+
+  // decrement
+  const removeValue = () => {
+    counter = counter - 1;
+    if(counter>=0)
+    setCounter(counter);
+  
+    console.log("value added", Math.random());
+  }
+  
+  return (
+    <>
+      <h1>Project 02 | chai aur code</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br/>
+      <button onClick={removeValue}>Remove value</button>
+
+      <p>footer : {counter}</p>
+
+    </>
+  )
+}
+
+export default App
